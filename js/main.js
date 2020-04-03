5c9dbddae9635b6f7d13735983ac3970d12dfcff
jose <jose.freire@linkbub.com>
2020-04-03 10:30:20 +0200

    Updated to 2020 holidays

--- js/main.js
+++ js/main.js
@@ -1,28 +1,46 @@
 var el = document.querySelector('#calc');
 var resultat = document.querySelector('.resultat');
-var festius2018 = ['2018-05-21', '2018-06-01', '2018-07-30', '2018-07-31', '2018-09-10', '2018-09-11', '2018-09-24', '2018-10-12', '2018-11-01', '2018-11-02', '2018-12-06', '2018-12-07', '2018-12-24', '2018-12-25', '2018-12-26', '2018-12-27', '2018-12-28', '2018-12-31'];
-var festius2019 = ['2019-01-01', '2019-01-02', '2019-01-03', '2019-01-04', '2019-04-18', '2019-04-19', '2019-04-22', '2019-05-01', '2019-05-02', '2019-05-03', '2019-06-10','2019-06-24','2019-09-09', '2019-09-10', '2019-09-11','2019-09-23','2019-09-24', '2019-10-31', '2019-11-01', '2019-12-05', '2019-12-06', '2019-12-23', '2019-12-24', '2019-12-25', '2019-12-26', '2019-12-27', '2019-12-30', '2019-12-31'];
-var festius2020 = ['2020-01-01', '2020-01-02', '2020-01-03', '2020-01-06', '2020-04-10', '2020-04-13'];
+var festius2018 = ['2018-05-21', '2018-06-01', '2018-07-30', '2018-07-31', '2018-09-10', '2018-09-11', '2018-09-24', 
+                    '2018-10-12', '2018-11-01', '2018-11-02', '2018-12-06', '2018-12-07', '2018-12-24', '2018-12-25', 
+                    '2018-12-26', '2018-12-27', '2018-12-28', '2018-12-31'];
+var festius2019 = ['2019-01-01', '2019-01-02', '2019-01-03', '2019-01-04', '2019-04-18', '2019-04-19', '2019-04-22', 
+                    '2019-05-01', '2019-05-02', '2019-05-03', '2019-06-10','2019-06-24','2019-09-09', '2019-09-10', 
+                    '2019-09-11','2019-09-23','2019-09-24', '2019-10-31', '2019-11-01', '2019-12-05', '2019-12-06', 
+                    '2019-12-23', '2019-12-24', '2019-12-25', '2019-12-26', '2019-12-27', '2019-12-30', '2019-12-31'];
+var festius2020 = ['2020-01-01', '2020-01-02', '2020-01-03', '2020-01-06', '2020-04-10', '2020-04-13','2020-05-01',
+                    '2020-06-24','2020-09-11','2020-09-25','2020-10-12','2020-11-01','2020-12-08','2020-12-25'];
+
 var festius_totals = festius2018.concat(festius2019, festius2020);
 
-(function ompleAgosts() {
-    for (var inici2018 = 20180801; inici2018 <= 20180831; inici2018++) {
+(function ompleAgosts() 
+{
+    for (var inici2018 = 20180801; inici2018 <= 20180831; inici2018++) 
+    {
         var data2018 = inici2018.toString();
         var dataguions_primer2018 = data2018.slice(0, 4) + "-" + data2018.slice(4);
         var dataguions_segon2018 = dataguions_primer2018.slice(0, 7) + "-" + dataguions_primer2018.slice(7);
         festius_totals.push(dataguions_segon2018);
     }
-    for (var inici2019 = 20190803; inici2019 <= 20190831; inici2019++) {
+    for (var inici2019 = 20190803; inici2019 <= 20190831; inici2019++) 
+    {
         var data2019 = inici2019.toString();
         var dataguions_primer2019 = data2019.slice(0, 4) + "-" + data2019.slice(4);
         var dataguions_segon2019 = dataguions_primer2019.slice(0, 7) + "-" + dataguions_primer2019.slice(7);
         festius_totals.push(dataguions_segon2019);
     }
+    for (var inici2020 = 20200803; inici2020 <= 20200831; inici2020++) 
+    {
+        var inici2020 = inici2020.toString();
+        var dataguions_primer2020 = inici2020.slice(0, 4) + "-" + inici2020.slice(4);
+        var dataguions_segon2020 = dataguions_primer2020.slice(0, 7) + "-" + dataguions_primer2020.slice(7);
+        festius_totals.push(dataguions_segon2020);
+    }
 })();
 
-if (el) {
-    el.addEventListener("click", function() {
-
+if (el) 
+{
+    el.addEventListener("click", function() 
+    {
         var dia_inici = document.querySelector('#dia_inici').value;
         var comprova = moment(dia_inici, 'DD/MM/YYYY', true).isValid();
         if (comprova == true) {
@@ -40,7 +58,8 @@ if (el) {
             // console.log("dies_queden = " + dies_queden);
 
 
-            if (dia_actual > dia_stop) {
+            if (dia_actual > dia_stop) 
+            {
                 setBarTo100();
                 resultat.innerHTML = 'Ep, tu ja has acabat el curs ! enhorabona!!! ;-)';
             } else if (dia_actual < dia_inici_oficial) {
